804ebadb4ce9141f5932d40c8c847100b14aebec
Lucas Queiroz <sepulchrolucas@gmail.com>
2023-04-26 00:41:34 -0300

    feat: handle navigation

--- src/modules/authentication/components/SignInFooter/index.tsx
+++ src/modules/authentication/components/SignInFooter/index.tsx
@@ -1,12 +1,19 @@
+import { useNavigation } from '@react-navigation/native'
 import { Text, View } from 'react-native'
+import { AuthStackParamList } from '../../../../routes/types/auth-route'
 
 export default function SignInFooter() {
+  const navigation = useNavigation<AuthStackParamList>()
+
   return (
     <View className="items-center justify-center space-y-8 px-6">
       <View>
         <Text className="font-inter-medium text-dark-gray-500 text-base">
           Don't have an account ?{' '}
-          <Text className="text-brand-violet-500 underline font-inter-bold">
+          <Text
+            className="text-brand-violet-500 underline font-inter-bold"
+            onPress={() => navigation.navigate('SignUp')}
+          >
             Sign Up
           </Text>
         </Text>
