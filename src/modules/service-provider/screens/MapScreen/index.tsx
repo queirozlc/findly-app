2ec0da875552f3a0e1488cfdb5f758d0cb48cfab
Lucas Queiroz <sepulchrolucas@gmail.com>
2023-05-01 10:40:55 -0300

    feat: map view for service provider module

--- src/modules/service-provider/screens/MapScreen/index.tsx
+++ src/modules/service-provider/screens/MapScreen/index.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import { useMap } from '../../../costumer/utils/hooks/useMap'
+import { mapStyle } from './styles'
 
 export default function MapScreen() {
+  const { mapRef } = useMap()
+
   return (
-    <View>
-      <Text>MapScreen</Text>
+    <View className="flex-1">
+      <MapView
+        ref={mapRef}
+        followsUserLocation
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation
+        customMapStyle={mapStyle}
+        className="w-full absolute inset-x-0 inset-y-0"
+      />
     </View>
   )
 }
