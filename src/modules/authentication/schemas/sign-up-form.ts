01a67b81ee9119f85b778664fc2af4dffd0ea1f3
Lucas Queiroz <sepulchrolucas@gmail.com>
2023-05-04 22:34:01 -0300

    feat: sign up validation schema

---
+++ src/modules/authentication/schemas/sign-up-form.ts
@@ -0,0 +1,26 @@
+import { z } from 'zod'
+
+export const SignUpFormSchema = z.object({
+  name: z.string().nonempty({
+    message: 'Name is required',
+  }),
+  email: z
+    .string()
+    .nonempty({
+      message: 'E-mail is required',
+    })
+    .email({
+      message: 'E-mail format is invalid',
+    }),
+  password: z
+    .string()
+    .nonempty({
+      message: 'Password is required',
+    })
+    .min(8, {
+      message: 'Password must be at least 8 characters long',
+    })
+    .max(32, {
+      message: 'Password must be at most 32 characters long',
+    }),
+})
